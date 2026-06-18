7c60484fd77ff87b3dc1d423c77664d3685cae87
newsdooly-alt <newsdooly@gmail.com>
2026-06-19 05:16:59 +0900

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,8 @@ const config: CapacitorConfig = {
   webDir: 'dist/public',
   server: {
     androidScheme: 'https',
+    url: 'https://economydino.com',
+    cleartext: false,
   },
   plugins: {
     SplashScreen: {
